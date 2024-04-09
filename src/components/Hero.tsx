a741d8b7933f2f7f5e087015a8c9bdd073f613ff
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-04-09 13:39:43 +0100

    update website intro

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,12 @@ const Hero = () => {
         <p className="text-[#77B255] text-xl sm:text-xl">👋 Hi there!</p>
         <h1 className="text-4xl sm:text-4xl font-bold">I'm Vishnu Kerasiya,</h1>
         <h2 className="text-3xl sm:text-2xl text-gray-600 w-[100%] text-justify">
-          a full-stack developer who loves coding and cares about the
-          environment and healthy eating. Join me on my website, where I share
-          my coding adventures and tips, along with some crunchy stories about
-          sustainable living and my favorite nutritious snacks. Let's make
-          coding and munching on good food simple and fun! 🌱💻
+          Welcome to my website! I'm a software developer who loves writing
+          code. When I'm not coding, I enjoy listening to music and watching
+          football. I'm also passionate about protecting the environment, So I'm
+          committed to crafting products that contribute positively to
+          environmental preservation. Stick around to learn more about me and
+          the things I'm interested in!
         </h2>
       </div>
     </div>

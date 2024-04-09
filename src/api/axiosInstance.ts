384308059f4f8bb4fa643d77060d9191a3285313
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-04-09 17:38:33 +0100

    add https cloudfront url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://43.204.154.26:5000/api", // Replace with your API base URL
+  baseURL: "https://d2gcgu77m9vqg6.cloudfront.net/api", // Replace with your API base URL
   // baseURL: "http://localhost:5000/api", // Replace with your API base URL
 
   timeout: 5000, // Set a timeout for requests (optional)

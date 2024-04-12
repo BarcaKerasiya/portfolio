09bdb6dea46d4de431d007d3f27a08b7fb5f9366
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-04-12 12:45:37 +0100

    remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-// import Hero from "./components/Hero";
-// import { Navbar } from "./components/Navbar";
 import BlogContentSection from "./components/Blog/BlogContent";
-// import Work from "./components/Work";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  createBrowserRouter,
-  // RouterProvider,
-  // Route,
-  // Link,
-} from "react-router-dom";
-// import Home from "./components/Hero";
-// import Skills from "./components/Skills";
+import { createBrowserRouter } from "react-router-dom";
 import Blog from "./components/Blog/Blog";
-// import ErrorPage from "./error-page";
 import Layout from "./Layout";
 import Hero from "./components/Hero";
 import Tech from "./components/Tech";
 import ErrorPage from "./error-page";
-
 export const router = createBrowserRouter([
   {
     path: "/",

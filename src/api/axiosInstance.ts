d35b7465ecc81045395eefe9e0db35c9dc2c7a05
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-07-22 08:58:40 +0100

    add discover latest blog in home page

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://d2gcgu77m9vqg6.cloudfront.net/api", // Replace with your API base URL
-  // baseURL: "http://localhost:5000/api", // Replace with your API base URL
+  // baseURL: "https://d2gcgu77m9vqg6.cloudfront.net/api", // Replace with your API base URL
+  baseURL: "http://localhost:5000/api", // Replace with your API base URL
 
   timeout: 5000, // Set a timeout for requests (optional)
   headers: {
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
       <div className="w-full lg:h-screen bg-[#0a192f] pt-[80px] sm:pt-40 md:pt-27 lg:pt-0 pb-8">
-        <div className="md:w-[80%] mx-auto px-8 flex flex-col justify-center h-full pt-0 sm:pt-27 md:pt-27">
+        <div className="w-[80%] md:w-[80%] mx-auto px-8 flex flex-col justify-center h-[80%] pt-0 sm:pt-27 md:pt-27">
           <p className="text-[#77B255] text-xl sm:text-xl">👋 Hi there!</p>
           <h1 className="text-4xl sm:text-4xl font-bold">
             I'm Vishnu Kerasiya,
@@ -16,6 +18,14 @@ const Home = () => {
             the things I'm interested in!
           </h2>
         </div>
+        <section className="flex justify-center m-4">
+          <Link
+            className="text-2xl font-medium text-white mb-4 underline decoration-green-500"
+            to="/blogs"
+          >
+            Discover Our Latest Blog Post!
+          </Link>
+        </section>
       </div>
     </>
   );

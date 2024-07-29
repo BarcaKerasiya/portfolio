aeb8003a494c4d13e1c8143ee7a4a3b5535512b4
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-07-29 08:19:41 +0100

    ham burger issue fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,7 @@
 import { FaBars, FaTimes } from "react-icons/fa";
-// import { HiOutlineMail } from "react-icons/hi";
-// import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo1.png";
 import { useState } from "react";
-// import { linkedin, github, mail } from "../constants/constants";
 import { NavLink } from "react-router-dom";
-// import resume from "../assets/pdfs/vishnu_kerasiya.pdf";
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState<boolean>(false);
@@ -13,79 +9,66 @@ export const Navbar = () => {
   const toggleClick = () => {
     setToggle(!toggle);
   };
-  return (
-    <div
-      // id="navbar"
-      className="flex justify-between items-center fixed h-[80px] bg-[#0a192f] w-full px-4"
-    >
-      <div>
-        <NavLink to="/">
-          <img src={Logo} className="h-11" />
-        </NavLink>
-      </div>
-      <ul className="hidden md:flex">
-        <li>
-          <NavLink className="hover:text-[#77B255] duration-300" to="/">
-            Home
-          </NavLink>
-        </li>
-        {/* <li>
-          <NavLink className="hover:text-[#77B255] duration-300" to="about">
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="hover:text-[#77B255] duration-300" to="skills">
-            Skills
-          </NavLink>
-        </li> */}
-        <li>
-          <NavLink className="hover:text-[#77B255] duration-300" to="/blogs">
-            Blogs
-          </NavLink>
-        </li>
-        {/* <li>
-          <NavLink
-            className="hover:text-[#77B255] duration-300"
-            to="work"
-            smooth={true}
-            offset={50}
-            duration={500}
-          >
-            Work
-          </NavLink>
-        </li> */}
-        <li>
-          <NavLink className="hover:text-[#77B255] duration-300" to="/tech">
-            Tech
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="hover:text-[#77B255] duration-300" to="/contact">
-            Contact
-          </NavLink>
-        </li>
-        {/* <li>
-          <NavLink className="hover:text-[#77B255] duration-300" to="/sign-in">
-            Sign In
-          </NavLink>
-        </li> */}
-      </ul>
-      <div onClick={toggleClick} className="md:hidden  z-10">
-        {!toggle ? (
-          <FaBars className="md:hidden cursor-pointer" />
-        ) : (
-          <FaTimes className="md:hidden cursor-pointer" />
-        )}
-      </div>
 
-      {/* mobile menu */}
+  return (
+    <div className="relative">
+      <div className="flex justify-between items-center h-[80px] bg-[#0a192f] w-full px-4">
+        <div>
+          <NavLink to="/">
+            <img src={Logo} className="h-11" />
+          </NavLink>
+        </div>
+        <ul className="hidden md:flex">
+          <li>
+            <NavLink className="hover:text-[#77B255] duration-300" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hover:text-[#77B255] duration-300" to="/blogs">
+              Blogs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hover:text-[#77B255] duration-300" to="/tech">
+              Tech
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="hover:text-[#77B255] duration-300"
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <div onClick={toggleClick} className="md:hidden z-10">
+          {!toggle ? (
+            <FaBars className="md:hidden cursor-pointer" />
+          ) : (
+            <FaTimes className="md:hidden cursor-pointer" />
+          )}
+        </div>
+      </div>
+      {/* Mobile Menu Overlay */}
+      {toggle && (
+        <div
+          className="z-50 fixed inset-0 bg-black bg-opacity-50"
+          onClick={toggleClick}
+        >
+          <FaTimes
+            className="md:hidden cursor-pointer absolute top-[40px] right-[40px]"
+            onClick={toggleClick}
+          />
+        </div>
+      )}
+      {/* Mobile Menu */}
+
       <ul
-        className={
-          !toggle
-            ? "hidden"
-            : "bg-slate-500 absolute top-0 left-0 h-screen w-full flex flex-col justify-center items-center md:hidden"
-        }
+        className={`fixed top-0 left-0 h-screen w-full flex flex-col justify-center items-center bg-[#0a192f] transition-transform duration-300 ${
+          toggle ? "translate-x-0" : "-translate-x-full"
+        } md:hidden z-50`}
       >
         <li className="py-6 text-2xl">
           <NavLink
@@ -96,50 +79,16 @@ export const Navbar = () => {
             Home
           </NavLink>
         </li>
-        {/* <li className="py-6 text-2xl">
-          {" "}
-          <NavLink
-            onClick={toggleClick}
-            to="about"
-            className="hover:text-[#77B255] duration-300"
-          >
-            About
-          </NavLink>
-        </li>
         <li className="py-6 text-2xl">
           <NavLink
             onClick={toggleClick}
-            to="skills"
-            className="hover:text-[#77B255] duration-300"
-          >
-            Skills
-          </NavLink>
-        </li> */}
-        <li className="py-6 text-2xl">
-          {" "}
-          <NavLink
-            onClick={toggleClick}
-            to="blogs"
+            to="/blogs"
             className="hover:text-[#77B255] duration-300"
           >
             Blogs
           </NavLink>
         </li>
-        {/* <li className="py-6 text-2xl">
-          {" "}
-          <NavLink
-            onClick={toggleClick}
-            to="work"
-            smooth={true}
-            offset={50}
-            duration={500}
-            className="hover:text-[#77B255] duration-300"
-          >
-            Work
-          </NavLink>
-        </li> */}
         <li className="py-6 text-2xl">
-          {" "}
           <NavLink
             onClick={toggleClick}
             to="/tech"
@@ -149,7 +98,6 @@ export const Navbar = () => {
           </NavLink>
         </li>
         <li className="py-6 text-2xl">
-          {" "}
           <NavLink
             onClick={toggleClick}
             to="/contact"
@@ -158,62 +106,7 @@ export const Navbar = () => {
             Contact
           </NavLink>
         </li>
-        {/* <li className="py-6 text-2xl">
-          {" "}
-          <NavLink
-            onClick={toggleClick}
-            to="/sign-in"
-            className="hover:text-[#77B255] duration-300"
-          >
-            Sign In
-          </NavLink>
-        </li> */}
       </ul>
-
-      {/* social media icons */}
-      {/* <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
-        <ul>
-          <li className="flex justify-center items-center w-[125px] h-[40px] ml-[-85px] hover:ml-[0px] duration-300 bg-blue-600 px-0 rounded-r-lg">
-            <a
-              href={linkedin}
-              target="_blank"
-              className="flex items-center justify-between w-full ml-2"
-            >
-              LinkedIn
-              <FaLinkedin size={25} className="mr-2" />
-            </a>
-          </li>
-          <li className="flex justify-center items-center w-[125px] h-[40px] ml-[-85px] hover:ml-[0px] duration-300 bg-[#333333] px-0 rounded-r-lg">
-            <a
-              href={github}
-              target="_blank"
-              className="flex items-center justify-between w-full ml-2"
-            >
-              Github
-              <FaGithub size={25} className="mr-2" />
-            </a>
-          </li>
-          <li className="flex justify-center items-center w-[125px] h-[40px] ml-[-85px] hover:ml-[0px] duration-300 bg-[#6fc2b0] px-0 rounded-r-lg">
-            <a
-              href={`mailto:${mail}`}
-              className="flex items-center justify-between w-full ml-2"
-            >
-              Email
-              <HiOutlineMail size={25} className="mr-2" />
-            </a>
-          </li>
-          <li className="flex justify-center items-center w-[125px] h-[40px] ml-[-85px] hover:ml-[0px] duration-300 bg-[#565f69] px-0 rounded-r-lg">
-            <a
-              href={resume}
-              target="_blank"
-              className="flex items-center justify-between w-full ml-2"
-            >
-              Resume
-              <BsFillPersonLinesFill size={25} className="mr-2" />
-            </a>
-          </li>
-        </ul>
-      </div> */}
     </div>
   );
 };
